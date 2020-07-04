bf59d8fb69cc678ad0ca5d6634edd0fc7d421c58
diego alen aguilera alvarado <daaguile@alumnos.inf.utfsm.cl>
2020-07-04 11:01:38 -0400

    Updates

---
+++ precache-manifest.b8e94d7cab6aa9358f4cca071afaf779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566f9bbb7b6ff8b47eec655ff099788f",
+    "url": "/prueba-app/index.html"
+  },
+  {
+    "revision": "70e1279e13282592759e",
+    "url": "/prueba-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76b6b4257746597d704c",
+    "url": "/prueba-app/static/js/2.8eb8d0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba-app/static/js/2.8eb8d0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e1279e13282592759e",
+    "url": "/prueba-app/static/js/main.52d3656f.chunk.js"
+  },
+  {
+    "revision": "720318cdc7317876f193",
+    "url": "/prueba-app/static/js/runtime-main.cead9267.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prueba-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
